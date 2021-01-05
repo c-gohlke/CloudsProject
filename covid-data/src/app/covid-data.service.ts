fa99582670e46cc5b4764ff29c6eedd394477820
clement <clement.gohlke@eurecom.fr>
2021-01-05 19:02:47 +0100

    add bar chart

--- covid-data/src/app/covid-data.service.ts
+++ covid-data/src/app/covid-data.service.ts
@@ -15,7 +15,13 @@ import { catchError, map } from "rxjs/operators";
 
 export class covidDataService {
   private user: User | undefined | null;
-  private DATA_ROUTE = "https://api.covid19api.com/summary"
+  private MONTHS: String[] = ["01", "02", "03", "04", "05", "06", "07",
+  "08", "09", "10", "11", "12"];
+
+  private DAYS: String[] = ["00", "01", "02", "03", "04", "05", "06", "07",
+  "08", "09", "10", "11", "12", "13", "14", "15", "16", "17",
+  "18", "19", "20", "21", "22", "23", "24", "25", "26", "27",
+  "28", "29", "30", "31"];
 
   constructor(
     private afAuth: AngularFireAuth,
@@ -62,7 +68,53 @@ export class covidDataService {
     this.router.navigate(["signin"]);
   }
 
-  loadCovidData(): Observable<CovidData>{
+  checkGlobalData(): Observable<boolean>{
+    return this.firestore.collection("covid_data").doc("global").valueChanges().pipe(
+      map(
+        (covidData: any) => {
+          if(covidData){
+            if(!covidData["lastUpdated"]){
+              return true;
+            }
+            // if lastUpdate happened more than a day ago, fetch new data
+            else if(new Date().getTime() - covidData["lastUpdated"].toDate().getTime()>1000*3600*24){
+              return true;
+            }
+            else{
+              return false
+            }
+          }
+          else{
+            return true;
+          }
+        }));
+  }
+
+  getGlobalData(){
+    const httpOptions = {
+      headers: new HttpHeaders({ "Content-Type": "application/json"})
+    };
+
+    return this.httpClient.get("https://api.covid19api.com/summary", httpOptions)
+  }
+
+  updateGlobalData(newData: CovidData){
+    this.firestore.collection("covid_data").doc("global").set({
+      activeConfirmed: newData.activeConfirmed,
+      deathRate: newData.deathRate,
+      lastUpdated: newData.lastUpdated,
+      newConfirmed: newData.newConfirmed,
+      newDeaths: newData.newDeaths,
+      newRecovered: newData.newRecovered,
+      recoveryRate: newData.recoveryRate,
+      totalConfirmed: newData.totalConfirmed,
+      totalDeaths: newData.totalDeaths,
+      totalRecovered: newData.totalRecovered,      
+    }, {merge: true})
+  }
+
+
+  loadGlobalData(): Observable<CovidData>{
     return this.firestore.collection("covid_data").doc("global").valueChanges().pipe(
       map(
         (covidData: any) => {
@@ -100,19 +152,77 @@ export class covidDataService {
     );
   }
 
-  checkUpdateData(): Observable<boolean>{
-    return this.firestore.collection("covid_data").doc("global").valueChanges().pipe(
+  checkCountryDailyData(): Observable<boolean>{
+    return this.firestore.collection("daily_data").doc("lastUpdate").valueChanges().pipe(
+      map(
+      (lastUpdateDoc: any) => {
+        if(lastUpdateDoc){
+          if(!lastUpdateDoc["lastUpdated"]){
+            return true;
+          }
+          // if lastUpdate happened more than a day ago, fetch new data
+          else if(new Date().getTime() - lastUpdateDoc["lastUpdated"].toDate().getTime()>1000*3600*24){
+            return true;
+          }
+          else{
+            return false
+          }
+        }
+        else{
+          return true;
+        }
+      })
+    );
+  }
+
+  getCountryDailyData(country: string){
+    const httpOptions = {
+      headers: new HttpHeaders({ "Content-Type": "application/json"})
+    };
+
+    return this.httpClient.get("https://api.covid19api.com/total/dayone/country/" + country, httpOptions)
+  }
+
+  updateCountryDailyData(country: string, date: Date, confirmed: number, recovered: number, deaths: number){
+    this.firestore.collection("daily_data").doc(this.toDateString(date)).collection("countries").doc(country).set(
+      {
+        confirmed: confirmed,
+        recovered: recovered,
+        deaths: deaths
+      },
+      {
+        merge: true
+      }
+    );
+  }
+
+  updateCountryDailyDataTimestamp(){
+    this.firestore.collection("daily_data").doc("lastUpdated").set(
+      {
+        lastUpdated: new Date(),
+      },
+      {
+        merge: true
+      }
+    );
+  }
+
+  checkGlobalDailyData(date: Date): Observable<boolean>{
+    return this.firestore.collection("daily_data").doc(this.toDateString(date)).valueChanges().pipe(
       map(
         (covidData: any) => {
-          // if lastUpdate happened more than a day ago, fetch new data
+          console.log("in checkGlobalDailyData, data is:")
+          console.log(covidData)
           if(covidData){
-            let fetchData: boolean = (new Date().getTime() - covidData["lastUpdated"].toDate().getTime())>1000*3600*24;
-            if(!covidData["lastUpdated"] || fetchData){
-              console.log("fetching new data");
+            if(!covidData["lastUpdated"]){
+              return true;
+            }
+            // if lastUpdate happened more than a day ago, fetch new data
+            else if(new Date().getTime() - covidData["lastUpdated"].toDate().getTime()>1000*3600*24){
               return true;
             }
             else{
-              return false;
+              return false
             }
           }
           else{
@@ -121,41 +231,109 @@ export class covidDataService {
         }));
   }
 
-  updateData(newData: CovidData){
-    this.firestore.collection("covid_data").doc("global").set({
-      activeConfirmed: newData["activeConfirmed"],
-      deathRate: newData.deathRate,
-      lastUpdated: newData.lastUpdated,
-      newConfirmed: newData.newConfirmed,
-      newDeaths: newData.newDeaths,
-      newRecovered: newData.newRecovered,
-      recoveryRate: newData.recoveryRate,
-      totalConfirmed: newData.totalConfirmed,
-      totalDeaths: newData.totalDeaths,
-      totalRecovered: newData.totalRecovered,      
-    }, {merge: true})
-  }
-
-  getCovidData(){
-    console.log("updateData called")
+  getGlobalDailyData(date: Date){
     const httpOptions = {
       headers: new HttpHeaders({ "Content-Type": "application/json"})
     };
 
-    return this.httpClient.get(this.DATA_ROUTE, httpOptions)
+    let tomorrow = new Date(date)
+    tomorrow.setDate(tomorrow.getDate() + 1)
+
+    let api_url: string = "https://api.covid19api.com/world?from=" + this.toDateString(date) + "T00:00:00Z&to=" +
+    this.toDateString(tomorrow) + "T00:00:00Z"
+
+    console.log("api_url for getting global daily data for day " + this.toDateString(date) + " is : " + api_url)
+
+    return this.httpClient.get(api_url, httpOptions)   
   }
 
-  private handleError(error: HttpErrorResponse): Observable<never>{
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error("An error occurred:", error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code. The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+  updateGlobalDailyData(newRecovered: number, newConfirmed: number, newDeaths: number, date: Date){
+    this.firestore.collection("daily_data").doc(this.toDateString(date)).set(
+      {
+        newConfirmed: newConfirmed,
+        newRecovered: newRecovered,
+        newDeaths: newDeaths,
+        lastUpdated: new Date()
+      },
+      {
+        merge: true
+      }
+    );
+  }
+
+  loadGlobalDailyData(date: Date): Observable<{newConfirmed: number, newRecovered: number, newDeaths: number}>{
+    return this.firestore.collection("daily_data").doc(this.toDateString(date)).valueChanges().pipe(
+      map(
+        (dailyData: any) => {
+          if(dailyData){
+            return {
+              newConfirmed: dailyData.newConfirmed,
+              newRecovered: dailyData.newRecovered,
+              newDeaths: dailyData.newDeaths,
+            }
+          }
+          else{
+            return {
+              newConfirmed: -1,
+              newRecovered: -1,
+              newDeaths: -1,
+            }
+          }
+      })
+    );
+  }
+
+  checkCountriesList(): Observable<boolean>{
+    return this.firestore.collection("countries").doc("countryList").valueChanges().pipe(
+      map(
+        (countryList: any) => {
+          if(countryList){
+            if(!countryList["lastUpdated"]){
+              return true;
+            }
+            // if lastUpdate happened more than a day ago, fetch new data
+            else if(new Date().getTime() - countryList["lastUpdated"].toDate().getTime()>1000*3600*24){
+              return true;
+            }
+            else{
+              return false
+            }
+          }
+          else{
+            return true;
+          }
+        })
       );
-    }
-    // return an observable with a user-facing error message
-    return throwError(error);
+  }
+
+  getCountriesList(){
+    const httpOptions = {
+      headers: new HttpHeaders({ "Content-Type": "application/json"})
+    };
+
+    return this.httpClient.get("https://api.covid19api.com/countries", httpOptions)
+  }
+
+  updateCountriesList(countryList: string[]){
+    this.firestore.collection("countries").doc("countryList").set({slugs: countryList}, {merge: true})
+    this.firestore.collection("countries").doc("countryList").set({lastUpdated: new Date()}, {merge: true})
+  }
+
+  loadCountriesList(){
+    return this.firestore.collection("countries").doc("countryList").valueChanges().pipe(
+      map(
+        (countryList: any) => {
+          if(countryList){
+            return countryList;
+          }
+          else{
+            return null
+          }
+      })
+    );
+  }
+
+  toDateString(date: Date): string{
+    return date.getFullYear() + "-" + this.MONTHS[date.getMonth()] + "-" + this.DAYS[date.getDate()];
   }
 }
--- covid-data/src/app/home/home.component.ts
+++ covid-data/src/app/home/home.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { covidDataService } from '../covid-data.service';
 import { CovidData } from '../covid-data.model';
-import { Label, monkeyPatchChartJsTooltip, monkeyPatchChartJsLegend } from 'ng2-charts';
-import { ChartOptions, ChartType } from 'chart.js';
+import { Label, monkeyPatchChartJsTooltip, monkeyPatchChartJsLegend, BaseChartDirective } from 'ng2-charts';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { createOfflineCompileUrlResolver } from '@angular/compiler';
-
+import { __importDefault } from 'tslib';
 
 @Component({
   selector: 'app-home',
@@ -12,16 +12,26 @@ import { createOfflineCompileUrlResolver } from '@angular/compiler';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
   activeConfirmed: number|undefined;
   deathRate: String|undefined;
   lastUpdated: any;
-  newConfirmed: number|undefined
-  newDeaths: number|undefined
-  newRecovered: number|undefined
+  newConfirmed: number|undefined;
+  newDeaths: number|undefined;
+  newRecovered: number|undefined;
   recoveryRate: String|undefined;
-  totalConfirmed: number|undefined
-  totalDeaths: number|undefined
-  totalRecovered: number|undefined
+  totalConfirmed: number|undefined;
+  totalDeaths: number|undefined;
+  totalRecovered: number|undefined;
+  totalConfirmedLastWeek: number[] = new Array(7);
+  totalRecoveredLastWeek: number[] = new Array(7);
+  totalDeathsLastWeek: number[] = new Array(7);
+  last7Confirmed: number[] = new Array(7)
+  last7Recovered: number[] = new Array(7)
+  last7Deaths: number[] = new Array(7)
+
+  globalDailyData: Array<{ newConfirmed: number, newRecovered: number, newDeaths: number, dateString: string }> = new Array(7);
 
   PIE_CHART_TITLE = 'Coronavirus Cases Distribution Worldwide';
 
@@ -36,17 +46,72 @@ export class HomeComponent implements OnInit {
   public pieChartData: any[] = [];
   public pieChartType: ChartType = 'pie';
 
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+    scales: { xAxes: [{}], yAxes: [{}] },
+    plugins: {
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      }
+    }
+  };
+  public barChartType: ChartType = 'bar';
+  public barChartLegend: boolean = true;
+
+
+  public last7days: Array<Date> = new Array(7)
+
+  public barChartData: any[] = [
+    {data:  this.last7Confirmed, label: "New Confirmed"},
+    {data:  this.last7Recovered, label: "New Recovered"},
+    {data:  this.last7Deaths, label: "New Deaths"}
+  ];
+  public barChartLabels: Label[] = new Array(7);
+
+  public sinceApril13: Array<Date> = this.getDaysArray(new Date("2020-04-13"), new Date())
+
+  getDaysArray(start:Date, end: Date): Array<Date> {
+    for(var arr=[],dt=new Date(start); dt<=end; dt.setDate(dt.getDate()+1)){
+        arr.push(new Date(dt));
+    }
+    return arr;
+  };
+
+  private MONTHS: String[] = ["01", "02", "03", "04", "05", "06", "07",
+  "08", "09", "10", "11", "12"];
+
+  private DAYS: String[] = ["00", "01", "02", "03", "04", "05", "06", "07",
+  "08", "09", "10", "11", "12", "13", "14", "15", "16", "17",
+  "18", "19", "20", "21", "22", "23", "24", "25", "26", "27",
+  "28", "29", "30", "31"];
+
+  toDateString(date: Date): string{
+    return date.getFullYear() + "-" + this.MONTHS[date.getMonth()] + "-" + this.DAYS[date.getDate()];
+  }
+
   constructor(public covidDataService: covidDataService) {
     monkeyPatchChartJsTooltip();
     monkeyPatchChartJsLegend();
+
+    this.last7days = [
+      new Date(new Date().setDate(new Date().getDate()-6)),
+      new Date(new Date().setDate(new Date().getDate()-5)),
+      new Date(new Date().setDate(new Date().getDate()-4)),
+      new Date(new Date().setDate(new Date().getDate()-3)),
+      new Date(new Date().setDate(new Date().getDate()-2)), 
+      new Date(new Date().setDate(new Date().getDate()-1)),
+      new Date()
+    ];
   }
 
   ngOnInit(): void {
-    this.covidDataService.checkUpdateData().subscribe((checkUpdateData: boolean)=>{
-      console.log("checking if need to update the data " + checkUpdateData)
+    this.covidDataService.checkGlobalData().subscribe((checkUpdateData: boolean)=>{
+      console.log("checking if need to update global data: " + checkUpdateData)
       if(checkUpdateData){
-        console.log("getting covid data ")
-        this.covidDataService.getCovidData().subscribe((res: any)=>{
+        this.covidDataService.getGlobalData().subscribe((res: any)=>{
+          console.log("getting global data from API")
+
           let tConfirmed: number = res["Global"]["TotalConfirmed"];
           let tDeaths: number = res["Global"]["TotalDeaths"];
           let tRecovered: number = res["Global"]["TotalRecovered"];
@@ -63,15 +128,14 @@ export class HomeComponent implements OnInit {
             totalDeaths: tDeaths,
             totalRecovered: tRecovered
           };
-          console.log("updating data " + newData)
-          this.covidDataService.updateData(newData);
+          this.covidDataService.updateGlobalData(newData);
+          console.log("updating global data")
         });
       }
     });
 
-    this.covidDataService.loadCovidData().subscribe((covidData: CovidData)=>{
-      console.log("loading data from firebase ")
-      console.log(covidData)
+    this.covidDataService.loadGlobalData().subscribe((covidData: CovidData)=>{
+      console.log("loading global data from firebase")
 
       this.activeConfirmed = covidData["activeConfirmed"];
       this.deathRate = (covidData["deathRate"]!*100).toFixed(2) + "%";
@@ -94,5 +158,98 @@ export class HomeComponent implements OnInit {
         }
       ];
     });
+
+    this.covidDataService.checkCountriesList().subscribe((checkUpdateCountriesList: boolean) =>{
+      console.log("checking if need to update countries list: " + checkUpdateCountriesList)
+      if(checkUpdateCountriesList){
+        this.covidDataService.getCountriesList().subscribe((countryObjList: any) => {
+          console.log("getting countries list from API")
+          let countrySlugList = []
+          for (let countryObj of countryObjList) {
+            countrySlugList.push(countryObj.Slug)
+          }
+          this.covidDataService.updateCountriesList(countrySlugList);
+          console.log("updating country list data")
+        });
+      }
+    });
+
+    // this.covidDataService.checkCountryDailyData().subscribe((updateBool: boolean)=>{
+    //   console.log(updateBool)
+    //   if(updateBool){
+    //     this.covidDataService.loadCountriesList().subscribe((countryList: any) => {
+    //       if(countryList){            
+    //         for (let country of ["france", "germany"]){
+    //         // for (let country of countryList.slugs){ //TODO
+    //           this.covidDataService.getCountryDailyData(country).subscribe((data: any)=>{
+    //             console.log("recieved data for country " + country + " of length " + data.length)
+    //             // for (var _i = 0; _i < data.length; _i++){
+    //             for (var _i = data.length-8; _i < data.length; _i++){
+    //               let dailyEntry = data[_i]
+    //               for (var _j = 0; _j<7; _j++){
+    //                 if(this.toDateString(new Date(dailyEntry.Date)) === this.last7days[_j]){
+    //                   this.globalDailyData[_j].newConfirmed = this.globalDailyData[_j].totalConfirmed + dailyEntry.Confirmed
+    //                   this.globalDailyData[_j].newRecovered = this.globalDailyData[_j].totalRecovered + dailyEntry.Recovered
+    //                   this.globalDailyData[_j].newDeaths = this.globalDailyData[_j].totalDeaths + dailyEntry.Deaths
+    //                   this.globalDailyData[_j].dateString = this.toDateString(new Date(dailyEntry.Date))
+    //                 }
+    //                 console.log()
+    //               }
+    //               this.covidDataService.updateCountryDailyData(country, new Date(dailyEntry.Date), dailyEntry.Confirmed, dailyEntry.Recovered, dailyEntry.Deaths);
+    //             }
+    //           });
+    //         }
+    //         this.covidDataService.updateGlobalDailyData(this.globalDailyData);
+    //         this.covidDataService.updateCountryDailyDataTimestamp();
+    //       }
+    //     });
+    //   }
+    // });
+
+    for (let day of this.getDaysArray(new Date("2020-04-13"), new Date())){ //load data since April 13th
+      this.covidDataService.checkGlobalDailyData(day).subscribe((checkGlobalDailyData: boolean) => {
+        console.log("checking global daily data for day " + day + ": " + checkGlobalDailyData)
+
+        if(checkGlobalDailyData){
+          this.covidDataService.getGlobalDailyData(day).subscribe((globalDailyData: any)=>{
+            console.log("getting global daily data for day " + day)
+            if(globalDailyData && globalDailyData.length==1){
+              this.covidDataService.updateGlobalDailyData(
+                globalDailyData[0].NewConfirmed,
+                globalDailyData[0].NewRecovered,
+                globalDailyData[0].NewDeaths,
+                day
+                )
+            }
+            else {
+              this.covidDataService.updateGlobalDailyData(
+                0,
+                0,
+                0,
+                day
+                )
+            }
+            console.log("updating global daily data for day " + day)
+            }
+          );
+        }
+      })
+    }
+
+    for (let _i of [0,1,2,3,4,5,6]){
+        this.covidDataService.loadGlobalDailyData(this.last7days[_i]).subscribe((dailyData: any)=>{
+        this.last7Confirmed[_i] = dailyData.newConfirmed
+        this.last7Recovered[_i] = dailyData.newRecovered
+        this.last7Deaths[_i] = dailyData.newDeaths
+
+        console.log(this.last7Confirmed)
+        this.barChartData = [
+          {data: this.last7Deaths, label: "New Deaths"},
+          {data: this.last7Recovered, label: "New Recovered"},
+          {data: this.last7Confirmed, label: "New Confirmed"}
+        ]
+      });
+      this.barChartLabels[_i] = this.toDateString(this.last7days[_i])
+    }
   }
 }
